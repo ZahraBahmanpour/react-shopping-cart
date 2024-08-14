dd11625f7acc10acf9e804bdef258a92067ed129
Sahra <sahra@Sahra-Mac.local>
2024-08-14 10:41:46 +0330

    remove extra link in NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  const navigate = useNavigate();
-
   return (
     <div>
       <h1>404 Not Found</h1>
       <Link to={"/"}>Back to home</Link>
-      <button onClick={() => navigate(-1)}>Back to home</button>
     </div>
   );
 }
