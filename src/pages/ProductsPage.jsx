7e251b4d01ac54a797b81d0b819d1c4bb74839fa
Sahra <sahra@Sahra-Mac.local>
2024-08-23 12:21:06 +0330

    redirect productsPage to login

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,12 @@
 import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 import productService from "../services/productService";
-import { useLoaderData, useLocation, useSearchParams } from "react-router-dom";
+import {
+  Navigate,
+  useLoaderData,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getProducts } from "../features/product/productSlice";
@@ -29,44 +34,51 @@ export default function ProductsPage() {
   if (error) {
     return <div>{error}</div>;
   }
+  const token = localStorage.getItem("ACCESS_TOKEN");
   return (
-    <div className="mt-8 m-5">
-      <input
-        type="text"
-        placeholder="search here..."
-        className="p-2 mb-2 border w-1/2"
-        value={searchParams.get("name") ?? ""}
-        onChange={(e) =>
-          setSearchParams((prev) => {
-            if (e.target.value) {
-              prev.set("name", e.target.value);
-            } else {
-              prev.delete("name");
+    <div>
+      {token ? (
+        <div className="mt-8 m-5">
+          <input
+            type="text"
+            placeholder="search here..."
+            className="p-2 mb-2 border w-1/2"
+            value={searchParams.get("name") ?? ""}
+            onChange={(e) =>
+              setSearchParams((prev) => {
+                if (e.target.value) {
+                  prev.set("name", e.target.value);
+                } else {
+                  prev.delete("name");
+                }
+                return prev;
+              })
             }
-            return prev;
-          })
-        }
-      />
-      <label>
-        Only show available products
-        <input
-          type="checkbox"
-          checked={searchParams.get("available") === "true" ? true : false}
-          onChange={(e) =>
-            setSearchParams((prev) => {
-              e.target.checked
-                ? prev.set("available", e.target.checked)
-                : prev.delete("available");
-              return prev;
-            })
-          }
-        />
-      </label>
-      <div className="grid grid-cols-4 gap-5">
-        {products.map((p) => (
-          <ProductCard key={p.id} product={p} />
-        ))}
-      </div>
+          />
+          <label>
+            Only show available products
+            <input
+              type="checkbox"
+              checked={searchParams.get("available") === "true" ? true : false}
+              onChange={(e) =>
+                setSearchParams((prev) => {
+                  e.target.checked
+                    ? prev.set("available", e.target.checked)
+                    : prev.delete("available");
+                  return prev;
+                })
+              }
+            />
+          </label>
+          <div className="grid grid-cols-4 gap-5">
+            {products.map((p) => (
+              <ProductCard key={p.id} product={p} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Navigate to={"/login"} />
+      )}
     </div>
   );
 }
